mantisbt-plugins/inline-history

commit 00512f37e2ed9ba3f34bd0d43624fd9b2bfd834b

Filter out 'Note Added' from inline history entries

InlineHistory/InlineHistory.php
@@ -162,7 +162,7 @@ class InlineHistoryPlugin extends MantisPlugin {
 
 		$t_access_level_needed = config_get( 'view_history_threshold' );
 		if ( access_has_bug_level( $t_access_level_needed, $p_bug_id ) ) {
-			$this->history = history_get_events_array( $p_bug_id );
+			$this->history = array_filter(history_get_events_array( $p_bug_id ), 'InlineHistory_Filter_Entries');
 		}
 
 		$this->display_entries(0);
@@ -276,3 +276,12 @@ class InlineHistoryPlugin extends MantisPlugin {
 	}
 }
 
+/**
+ * Filter out history entries for "Note Added".
+ *
+ * @param History entry
+ * @return True if entry not "Note Added: xyz"
+ */
+function InlineHistory_Filter_Entries( $p_entry ) {
+	return (stristr($p_entry['note'], 'Note Added:') != $p_entry['note']);
+}

commit 9572e228eda36f8d88e9dc31348d44c8a3ec79f0

'Note added' filter does not respect language
Bug introduced in 00512f37e2ed9ba3f34bd0d43624fd9b2bfd834b

Signed-off-by: Damien Regad <dregad@mantisbt.org>

Edited commit message. Ref: Pull request #1

InlineHistory/InlineHistory.php
@@ -289,5 +289,5 @@ class InlineHistoryPlugin extends MantisPlugin {
  * @return True if entry not "Note Added: xyz"
  */
 function InlineHistory_Filter_Entries( $p_entry ) {
-	return (stristr($p_entry['note'], 'Note Added:') != $p_entry['note']);
+	return (stristr($p_entry['note'], lang_get( 'bugnote_added' ).':') != $p_entry['note']);
 }